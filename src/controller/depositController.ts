933e4599fa81a302cce2d971cf082ad1dad6fd81
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-04-26 10:30:26 -0300

    New routes for deposits

---
+++ src/controller/depositController.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import { AccountService } from "../services/account.service";
+import { DepositService } from "../services/deposit.service";
+
+export class DepositController {
+    public accountService: AccountService;
+    public depositService: DepositService;
+
+    constructor() {
+        this.accountService = new AccountService();
+        this.depositService = new DepositService();
+        this.Deposit = this.Deposit.bind(this); // Add this line
+    }
+
+    public async Deposit(req: Request, res: Response, next: NextFunction) {
+        const { id, value } = req.body;
+        if (!id || !value) {
+            return res.status(400).json({ message: "Missing fields" });
+        }
+        try {
+            const userFound = await this.accountService.getUserById(id);
+            if (userFound.status === false) {
+                return res.status(400).json({ message: "User deactivated" });
+            }
+            const newBalance = await this.depositService.Deposit(userFound, value);
+            return res.status(200).json({ message: "Deposit successfully", balance: newBalance });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { AccountController } from "../controller/accountController";
+import { DepositController } from "../controller/depositController";
 
 
 const router = Router();
 const accountController = new AccountController();
-
+const depositController = new DepositController();
 
 //Account Routes
 router.get("/account/count", accountController.getCount)
@@ -12,5 +13,7 @@ router.post("/account/new", accountController.CreateUser)
 router.delete("/account/desativate/:id", accountController.DesativateAccount)
 router.get("/account/get/:id", accountController.GetUserById)
 
+//Deposits Routes
+router.post("/deposit", depositController.Deposit)
 
 export { router }
